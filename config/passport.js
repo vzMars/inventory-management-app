2038a51dd8e5d2335803c4308b4cf96d27e039a7
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2022-09-04 21:53:36 -0400

    added passport config and database connection

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
+
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: 'email' },
+      async (email, password, done) => {
+        try {
+          const user = await User.findOne({
+            email: email.toLowerCase(),
+          }).exec();
+
+          if (!user) {
+            return done(null, false, { msg: `Email ${email} not found.` });
+          }
+
+          const isMatch = await user.comparePassword(password);
+
+          if (isMatch) {
+            return done(null, user);
+          }
+
+          return done(null, false, { msg: 'Invalid email or password.' });
+        } catch (err) {
+          return done(err);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
+  });
+};
